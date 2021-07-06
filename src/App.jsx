0a760376c52b9b5aba69a90e4d5002b077a01767
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-06 12:10:43 -0500

    Add course's components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,92 @@
+import { useState } from 'react';
 import './App.css';
 
-const App = () => {
+
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
   return (
-    <div className="App">
-      
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
+    </div>
+  );
+}
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Todo 1",
+      isCompleted: false
+    },
+    {
+      text: "Todo 2",
+      isCompleted: false
+    },
+    {
+      text: "Todo 3",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [{ text }, ...todos];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
